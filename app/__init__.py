7cbef41b1ec4c18450b51f728c176058e04eb31d
Gina <gina10111@hotmail.com>
2021-08-15 11:59:06 -0500

    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    Response,
+    session,
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -22,7 +31,6 @@ migrate = Migrate(app, db)
 app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-# fmt: off
 """class Task(db.Model):
     __tablename__ = 'task'
     id = db.Column(db.Integer, primary_key=True)
@@ -30,20 +38,14 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     done = db.Column(db.Boolean, default=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     #owner = d.Column(d.Text, nullable=False)
-
     def __init__(self, content, owner):
         self.content = content
         self.done = False
         self.owner = owner
-
     def __repr__(self):
         return '<Content %s>' % self.owner
-"""
-# fmt: on
-
-
 class UserModel(db.Model):
-    __tablename__ = "user"
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String())
     password = db.Column(db.String())
@@ -53,9 +55,8 @@ class UserModel(db.Model):
     health = db.Column(db.Integer)
     hunger = db.Column(db.Integer)
     point = db.Column(db.Integer)
-    # tasks_id = db.Column(db.ForeignKey(Task.id))
-    # tasks = db.relationship(Task, backref='owner')
-
+    #tasks_id = db.Column(db.ForeignKey(Task.id))
+   # tasks = db.relationship(Task, backref='owner')
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -65,29 +66,67 @@ class UserModel(db.Model):
         self.point = 0
         self.name = "hi"
         self.type = "cat"
-
     def __repr__(self):
         return f"<User {self.username}>"
+"""
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    done = db.Column(db.Boolean, default=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    # owner = d.Column(d.Text, nullable=False)
+
+    def __init__(self, content, owner):
+        self.content = content
+        self.done = False
+        self.owner = owner
+
+    def __repr__(self):
+        return "<Content %s>" % self.owner
+
+
+class User(db.Model):
+    # __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    level = db.Column(db.Integer)
+    name = db.Column(db.Text)
+    type = db.Column(db.Text)
+    health = db.Column(db.Integer)
+    hunger = db.Column(db.Integer)
+    point = db.Column(db.Integer)
+    # tasks_id = db.Column(db.ForeignKey(Task.id))
+    tasks = db.relationship(Task, backref="owner")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.level = 0
+        self.name = "Pretzel"
+        self.type = "cat"
+        self.hunger = 100
+        self.health = 100
+        self.point = 0
 
 
 with app.app_context():
 
     @app.route("/")
     def main():
-        # fmt: off
         """
         Returns the main page of the app, where basic info and options as well as contact
         is provided
         """
-        # fmt: on
         return render_template("index.html", title="Intro Screen", url=os.getenv("URL"))
 
     def error_caller(error):
-        # fmt: off
         """
         Reusage function to recycle code
         """
-        # fmt: on
         if error == "username":
             return "Username is required."
         elif error == "password":
@@ -97,11 +136,10 @@ with app.app_context():
 
     @app.route("/register", methods=["GET", "POST"])
     def register():
-        # fmt: off
         """
         Regiters into db the new user with username and  hashes with sha-21 the password
         """
-        # fmt: on
+
         if request.method == "POST":
             username = request.form.get("username")
             password = request.form.get("password")
@@ -114,12 +152,12 @@ with app.app_context():
                 error = error_caller("password")
             if password != confPassword:
                 error = error_caller("confPassword")
-            elif UserModel.query.filter_by(username=username).first() is not None:
+            elif User.query.filter_by(username=username).first() is not None:
                 error = f"User {username} already exists"
                 print(error, "ALL CLEAR HERE---")
 
-            if error is None:
-                new_username = UserModel(username, generate_password_hash(password))
+            if error == None:
+                new_username = User(username, generate_password_hash(password))
                 db.session.add(new_username)
                 db.session.commit()
                 # return f"User {username} created successfully"
@@ -139,11 +177,9 @@ with app.app_context():
 
     @app.route("/login", methods=["GET", "POST"])
     def login():
-        # fmt: off
         """
         Checks for input and when given, checks for the information in the database
         """
-        # fmt: on
         if request.method == "POST":
             username = request.form.get("username")
             password = request.form.get("password")
@@ -155,7 +191,7 @@ with app.app_context():
                 error = error_caller("password")
 
             # check for the user in the database
-            user_ = UserModel.query.filter_by(username=username).first()
+            user_ = User.query.filter_by(username=username).first()
 
             if not user_:
                 error = "Nonexistent or incorrect username"
@@ -167,7 +203,7 @@ with app.app_context():
                 return render_template(
                     "login.html", title="Login", url=os.getenv("URL")
                 )
-
+            session["username"] = username
             return redirect(url_for("todo"))
 
         else:
@@ -175,21 +211,69 @@ with app.app_context():
 
     @app.route("/health")
     def healthy():
-        # fmt: off
         """
         Health function for life checking
         """
-        # fmt: on
         return "Healthy as it should."
 
-    @app.route("/todo")
+    @app.route("/logout")
+    def logout():
+        del session["username"]
+        return redirect("/")
+
+    @app.route("/todo", methods=["GET", "POST"])
     def todo():
-        # fmt: off
         """
         Health function for life checking
         """
-        # fmt: on
-        return render_template("todo.html", title="To Do", url=os.getenv("URL"))
+
+        username = session["username"]
+        owner = User.query.filter_by(username=username).first()
+
+        # tasks = Task.query.filter_by(done=False, owner=owner).all()
+        tasks = Task.query.filter_by(owner=owner).all()
+        # completed_tasks = Task.query.filter_by(done=True, owner=owner).all()
+        # tasks = Task.query.all()
+        return render_template("todo.html", tasks=tasks, url=os.getenv("URL"))
+
+    @app.route("/task", methods=["POST"])
+    def add_task():
+        content = request.form["content"]
+        if not content:
+            return "Error"
+
+        username = session["username"]
+        owner = User.query.filter_by(username=username).first()
+        new_task = Task(content, owner)
+        db.session.add(new_task)
+        db.session.commit()
+
+        return redirect(url_for("todo"))
+
+    @app.route("/delete/<int:task_id>")
+    def delete_task(task_id):
+        task = Task.query.get(task_id)
+        if not task:
+            return redirect("/")
+
+        db.session.delete(task)
+        db.session.commit()
+        return redirect(url_for("todo"))
+
+    @app.route("/done/<int:task_id>")
+    def resolve_task(task_id):
+        task = Task.query.get(task_id)
+
+        if not task:
+            return redirect("/")
+        if task.done:
+            task.done = False
+        # after resolving add points to the user
+        else:
+            task.done = True
+
+        db.session.commit()
+        return redirect(url_for("todo"))
 
 
 if __name__ == "__main__":
