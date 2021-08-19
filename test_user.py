db48829e9edc7225dd0bca95b2508ca735546600
Gina <gina10111@hotmail.com>
2021-08-19 12:51:33 -0500

    reformat and change command

--- test_user.py
+++ test_user.py
@@ -7,9 +7,9 @@ def test_new_user():
     WHEN a new User is created
     THEN check the email, hashed_password, and role fields are defined correctly
     """
-    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
-    assert user.username == 'patkennedy79@gmail.com'
-    assert user.password == 'FlaskIsAwesome'
+    user = User("patkennedy79@gmail.com", "FlaskIsAwesome")
+    assert user.username == "patkennedy79@gmail.com"
+    assert user.password == "FlaskIsAwesome"
     assert user.level == 0
     assert user.name == "Pretzel"
     assert user.type == "cat"
