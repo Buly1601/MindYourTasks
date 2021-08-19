8ebda5a5e973742eedc5dedf92264c57fa84bd80
ayredkklc <ayredkklc@gmail.com>
2021-08-19 17:01:54 -0400

    added main functions to the code

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash, Res
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import math
+import time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
@@ -31,12 +33,14 @@ migrate = Migrate(app, db)
         return f"<User {self.username}>"
 '''
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     done = db.Column(db.Boolean, default=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    #owner = d.Column(d.Text, nullable=False)
+
+    # owner = d.Column(d.Text, nullable=False)
 
     def __init__(self, content, owner):
         self.content = content
@@ -48,7 +52,7 @@ class Task(db.Model):
 
 
 class User(db.Model):
-    #__tablename__ = "users"
+    # __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120))
@@ -59,7 +63,9 @@ class User(db.Model):
     health = db.Column(db.Integer)
     hunger = db.Column(db.Integer)
     point = db.Column(db.Integer)
-    #tasks_id = db.Column(db.ForeignKey(Task.id))
+    allpoints = db.Column(db.Integer)
+    status = db.Column(db.Boolean, default=True)
+    # tasks_id = db.Column(db.ForeignKey(Task.id))
     tasks = db.relationship(Task, backref='owner')
 
     def __init__(self, username, password):
@@ -71,6 +77,9 @@ class User(db.Model):
         self.hunger = 100
         self.health = 100
         self.point = 0
+        self.allpoints = 0
+        self.status = True
+
 
 '''class PostModel(db.Model):
     __tablename__ = "post"
@@ -84,12 +93,13 @@ class User(db.Model):
     img_name = db.Column(db.Text, nullable=False)
     img_mimetype = db.Column(db.Text, nullable=False)'''
 
+# db.create_all()
+# db.session.add(Task(paths=[DomainPath(), DomainPath()]))
+# db.session.commit()
 
-#db.create_all()
-#db.session.add(Task(paths=[DomainPath(), DomainPath()]))
-#db.session.commit()
-
-#d.create_all() do flask db init instead
+# d.create_all() do flask db init instead
+class TimerError(Exception):
+   """A custom exception used to report errors in use of Timer class"""
 
 with app.app_context():
     @app.route("/")
@@ -138,7 +148,7 @@ with app.app_context():
                 new_username = User(username, generate_password_hash(password))
                 db.session.add(new_username)
                 db.session.commit()
-                #return f"User {username} created successfully"
+                # return f"User {username} created successfully"
                 return redirect(url_for('login'))
 
             else:
@@ -176,11 +186,11 @@ with app.app_context():
                 flash(error)
                 return render_template("login.html", title="Login", url=os.getenv("URL"))
             session["username"] = username
+            start = time.time()
             return redirect(url_for('todo'))
 
         else:
             return render_template("login.html", title="Login", url=os.getenv("URL"))
-
 
 
     @app.route('/logout')
@@ -188,13 +198,14 @@ with app.app_context():
         del session['username']
         return redirect('/')
 
+
     @app.route("/health")
     def healthy():
         """
         Health function for life checking
         """
         return "Healthy as it should."
-    
+
 
     @app.route("/todo", methods=["GET", "POST"])
     def todo():
@@ -205,10 +216,10 @@ with app.app_context():
         username = session['username']
         owner = User.query.filter_by(username=username).first()
 
-        #tasks = Task.query.filter_by(done=False, owner=owner).all()
+        # tasks = Task.query.filter_by(done=False, owner=owner).all()
         tasks = Task.query.filter_by(owner=owner).all()
-        #completed_tasks = Task.query.filter_by(done=True, owner=owner).all()
-        #tasks = Task.query.all()
+        # completed_tasks = Task.query.filter_by(done=True, owner=owner).all()
+        # tasks = Task.query.all()
         return render_template('todo.html', tasks=tasks, url=os.getenv("URL"))
 
 
@@ -223,6 +234,14 @@ with app.app_context():
         new_task = Task(content, owner)
         db.session.add(new_task)
         db.session.commit()
+        if owner.hunger == 0:
+            if owner.health == 10:
+                owner.level = 0
+                owner.point = 0
+                owner.hunger = 100
+                owner.health = 100
+            else:
+                owner.health -= 10
 
         return redirect(url_for('todo'))
 
@@ -241,17 +260,66 @@ with app.app_context():
     @app.route('/done/<int:task_id>')
     def resolve_task(task_id):
         task = Task.query.get(task_id)
+        username = session['username']
+        owner = User.query.filter_by(username=username).first()
 
         if not task:
             return redirect('/')
         if task.done:
             task.done = False
-        #after resolving add points to the user
+            owner.point -= 1
+            owner.allpoints -= 1
+        # after resolving add points to the user
         else:
             task.done = True
+            owner.point += 1
+            owner.allpoints += 1
+        # after owner gets to a point they level up
+        tot = math.floor(owner.allpoints / 10)
+        owner.level = tot
 
         db.session.commit()
         return redirect(url_for('todo'))
+
+    # for feeding the pet
+    @app.route('/feed', methods=['POST'])
+    def feed():
+        error = None
+        username = session['username']
+        owner = User.query.filter_by(username=username).first()
+
+        if owner.point < 5:
+            error = "Not enough points"
+
+        else:
+            owner.point -= 5
+            owner.hunger += 5
+
+        # after resolving add points to the user
+        db.session.commit()
+        return redirect(url_for('todo'))
+
+    # for changing the pets name
+    @app.route('/change', methods=['POST'])
+    def change_name():
+        petname = request.form['name']
+        if not petname:
+            return 'Error'
+
+        username = session['username']
+        owner = User.query.filter_by(username=username).first()
+
+        if not username:
+            return redirect('/')
+        # if owner.name != petname:
+        owner.name = petname
+        # after resolving add points to the user
+        db.session.commit()
+        return redirect(url_for('todo'))
+
+
+    def start(self):
+        """Start a new timer"""
 
 
 if __name__ == "__main__":
